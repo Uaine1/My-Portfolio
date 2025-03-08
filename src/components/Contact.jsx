2f56efd885a98a083d08878d46191ff98462a771
Uaine1 <angeloalcantara243@gmail.com>
2025-03-08 10:59:34 +0800

    Add animation using framer motion

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
+import { motion } from "motion/react"
 
 import {CONTACT} from '../constants'
 
 function Contact() {
   return (
     <div className='border-b border-neutral-900 pb-4'>
-        <h2 className='my-10 mb-2 text-center text-4xl'>Contacts</h2>
-        <p className='mt-2 my-10 text-center text-medium'>Interested? Reach me out on the following</p>
-        <div className='text-center tracking-tighter'>
+        <motion.h2 
+        whileInView={{opacity: 1, y: 0}}
+        initial={{opacity: 0, y: -100}}
+        transition={{duration: 1.5}}
+        className='my-10 mb-2 text-center text-4xl'>Contacts</motion.h2>
+        <motion.p 
+        whileInView={{opacity: 1, y: 0}}
+        initial={{opacity: 0, y: -100}}
+        transition={{duration: 1.3}}
+        className='mt-2 my-10 text-center text-medium'>Interested? Reach me out on the following</motion.p>
+        <motion.div 
+        whileInView={{opacity: 1, x: 0}}
+        initial={{opacity: 0, x: -100}}
+        transition={{duration: 1}} 
+        className='text-center tracking-tighter'>
             <p className='my-4'>{CONTACT.address}</p>
             <p className='my-4'>{CONTACT.phoneNo}</p>
             <a href="#" className='border-b'>{CONTACT.email}</a>
-        </div>
+        </motion.div>
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { motion } from "motion/react"
 
 import { HERO_CONTENT } from '../constants'
 import profilePic from "../assets/kevinRushProfile.png"
+
+const container = (delay) => ({
+    hidden: {x: -100, opacity: 0},
+    visible: {x: 0, opacity: 1, transition: {duration: 1, delay: delay}}
+})
 
 function Hero() {
   return (
@@ -9,20 +15,39 @@ function Hero() {
         <div className='flex flex-wrap'>
             <div className='w-full lg:w-1/2'>
                 <div className='flex flex-col items-center lg:items-start'>
-                    <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>
-                        Hir
-                    </h1>
-                    <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>
+                    <motion.h1 
+                    whileInView={{opacity: 1, x: 0}}
+                    initial={{opacity: 0, x: -100}}
+                    transition={{duration: 1}} 
+                    //animate="visible"
+                    className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>
+                        Jhon Wuthering
+                    </motion.h1>
+                    <motion.span
+                    whileInView={{opacity: 1, x: 0}}
+                    initial={{opacity: 0, x: -100}}
+                    transition={{duration: 1}} 
+                    //animate="visible" 
+                    className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'>
                         Ligma
-                    </span>
-                    <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>
+                    </motion.span>
+                    <motion.p
+                    whileInView={{opacity: 1, x: 0}}
+                    initial={{opacity: 0, x: -100}}
+                    transition={{duration: 1}} 
+                    //animate="visible"
+                    className='my-2 max-w-xl py-6 font-light tracking-tighter'>
                         {HERO_CONTENT}
-                    </p>
+                    </motion.p>
                 </div>
             </div>
             <div className='w-full lg:w-1/2 lg:p-8'>
                 <div className='flex justify-center'>
-                    <img src={profilePic} alt="profile pic"/>
+                    <motion.img
+                    whileInView={{opacity: 1, x: 0}}
+                    initial={{opacity: 0, x: 100}}
+                    transition={{duration: 1}} 
+                    src={profilePic} alt="profile pic"/>
                 </div>
             </div>
         </div>
